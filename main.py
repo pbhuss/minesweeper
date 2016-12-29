0556bd2e91d95b1348896ea94237f7493404f53d
Peter Huss <pbhuss@yelp.com>
2016-12-28 22:47:12 -0800

    1. Improved UI layout 2. Added music support 3. Added minesweeper solver (WIP) 4. Move static variables to config.yaml

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import enum
+import itertools
 import pygame
 import random
 import os
 import sys
 import time
+import yaml
+
+
+CONFIG = yaml.load(open('config.yaml'))
+DIFFICULTIES = CONFIG['difficulties']
+MUSIC = CONFIG['music']
+KEY_BINDINGS = CONFIG['key_bindings']
+MOUSE_BINDINGS = CONFIG['mouse_bindings']
+TILESET = CONFIG['tileset']
 
 
 def main():
     view = MinesweeperView()
+    solver_enabled = False
+
     while True:
         ev = pygame.event.get()
         for event in ev:
@@ -17,49 +29,30 @@ def main():
                 view.handle_click(event)
             if event.type == pygame.KEYUP:
                 view.handle_keyup(event)
+                if event.key == pygame.K_s:
+                    solver_enabled = not solver_enabled
         view.redraw() # for clock update
-
-
-@enum.unique
-class Difficulty(enum.Enum):
-    easy = {
-        'width': 9,
-        'height': 9,
-        'mines': 10,
-    }
-    medium = {
-        'width': 16,
-        'height': 16,
-        'mines': 40,
-    }
-    hard = {
-        'width': 30,
-        'height': 16,
-        'mines': 99,
-    }
-
-
-@enum.unique
-class Tileset(enum.Enum):
-    small = {
-        'dir': 'img_small/',
-        'tile_px': 21
-    }
-    large = {
-        'dir': 'img/',
-        'tile_px': 61
-    }
+        if solver_enabled:
+            solver = MinesweeperSolver(view.minesweeper)
+            soln = solver.solve_one()
+            if soln is not None:
+                if soln['action'] == 'flag':
+                    for coord in soln['coords']:
+                        view.minesweeper.toggle_flag(*coord)
+                elif soln['action'] == 'reveal':
+                    for coord in soln['coords']:
+                        view.minesweeper.reveal(*coord)
 
 
 class MinesweeperView(object):
 
-    def __init__(self, difficulty=Difficulty.easy, tileset=Tileset.small):
+    def __init__(self, difficulty='easy', tileset='small'):
         self._set_difficulty(difficulty, init=True)
         self._set_tileset(tileset, init=True)
         self.start_new_game()
 
     def start_new_game(self):
-        self.minesweeper = Minesweeper(**self.difficulty.value)
+        self.minesweeper = Minesweeper(**DIFFICULTIES[self.difficulty])
         self.start_time = None
         self.end_time = None
         self.redraw()
@@ -70,13 +63,16 @@ class MinesweeperView(object):
         black = 0, 0, 0
         white = 255, 255, 255
         self.screen.fill(black)
-        tile_px = self.tileset.value['tile_px']
+        tile_px = TILESET[self.tileset]['tile_px']
         for i in range(self.minesweeper.width):
             for j in range(self.minesweeper.height):
                 rect = (tile_px * i, tile_px * j)
                 square = self.minesweeper._squares[i, j]
                 if square.flagged:
-                    tile_name = 'flag'
+                    if self.minesweeper.state in MinesweeperState.end_states() and not square.is_mine:
+                        tile_name = 'mine_hit'
+                    else:
+                        tile_name = 'flag'
                 elif square.revealed:
                     if square.is_mine:
                         if square.hit:
@@ -89,12 +85,13 @@ class MinesweeperView(object):
                     tile_name = 'unexplored'
                 self.screen.blit(self.tiles[tile_name], rect)
 
-        font = pygame.font.SysFont('Consolas', 15, bold=1)
+        font_px = TILESET[self.tileset]['font_px']
+        font = pygame.font.SysFont('Consolas', font_px, bold=1)
         flag_remaining = font.render(str(self.minesweeper.flag_remaining), 1, white)
         self.screen.blit(
-            self.tiles['flag'], (40-21-10, self.minesweeper.height * tile_px + 15 - 3))
+            self.tiles['flag'], (0.5 * tile_px, (self.minesweeper.height + 0.5) * tile_px))
         self.screen.blit(
-            flag_remaining, (40, self.minesweeper.height * tile_px + 15))
+            flag_remaining, (1.8 * tile_px, (self.minesweeper.height + 1) * tile_px - font_px / 2))
         if self.start_time is None:
             seconds = 0
         elif self.minesweeper.state in MinesweeperState.end_states():
@@ -105,62 +102,89 @@ class MinesweeperView(object):
             seconds = int(time.time() - self.start_time)
         time_elapsed = font.render(str(seconds), 1, white)
         self.screen.blit(
-            self.tiles['clock'], (150-21-10, self.minesweeper.height * tile_px + 15 - 3))
+            self.tiles['clock'], ((self.minesweeper.width - 3) * tile_px, (self.minesweeper.height + 0.5) * tile_px))
         self.screen.blit(
-            time_elapsed, (150, self.minesweeper.height * tile_px + 15))
+            time_elapsed, ((self.minesweeper.width - 1.7) * tile_px, (self.minesweeper.height + 1) * tile_px - font_px / 2))
+
+        if self.minesweeper.state is MinesweeperState.game_won:
+            face_tile = self.tiles['win']
+        elif self.minesweeper.state is MinesweeperState.game_lost:
+            face_tile = self.tiles['lose']
+        else:
+            face_tile = self.tiles['normal']
+        self.screen.blit(
+            face_tile, ((self.minesweeper.width - 1) * tile_px / 2, (
+                self.minesweeper.height + 0.5) * tile_px))
 
         pygame.display.flip()
 
     def handle_click(self, event):
         mouse_x, mouse_y = event.pos
-        tile_px = self.tileset.value['tile_px']
+        tile_px = TILESET[self.tileset]['tile_px']
         i = int(mouse_x / tile_px)
         j = int(mouse_y / tile_px)
         if i >= self.minesweeper.width or j >= self.minesweeper.height:
             return
-        if event.button == 1:
+        if event.button == MOUSE_BINDINGS['reveal']:
             if self.minesweeper.state is MinesweeperState.pregame:
                 self.start_time = time.time()
             self.minesweeper.reveal(i, j)
-        elif event.button == 2:
+        elif event.button == MOUSE_BINDINGS['burst_reveal']:
             self.minesweeper.burst_reveal(i, j)
-        elif event.button == 3:
+        elif event.button == MOUSE_BINDINGS['flag']:
             self.minesweeper.toggle_flag(i, j)
         self.redraw()
 
     def handle_keyup(self, event):
-        if event.key == pygame.K_n:
+        if event.key == getattr(pygame, KEY_BINDINGS['new_game']):
             self.start_new_game()
-        elif event.key == pygame.K_1:
-            if self._set_difficulty(Difficulty.easy):
+        elif event.key == getattr(pygame, KEY_BINDINGS['difficulty_1']):
+            if self._set_difficulty('easy'):
                 self.start_new_game()
-        elif event.key == pygame.K_2:
-            if self._set_difficulty(Difficulty.medium):
+        elif event.key == getattr(pygame, KEY_BINDINGS['difficulty_2']):
+            if self._set_difficulty('medium'):
                 self.start_new_game()
-        elif event.key == pygame.K_3:
-            if self._set_difficulty(Difficulty.hard):
+        elif event.key == getattr(pygame, KEY_BINDINGS['difficulty_3']):
+            if self._set_difficulty('hard'):
                 self.start_new_game()
-        elif event.key == pygame.K_MINUS:
-            self._set_tileset(Tileset.small)
+        elif event.key == getattr(pygame, KEY_BINDINGS['difficulty_4']):
+            if self._set_difficulty('ultimate'):
+                self.start_new_game()
+        elif event.key == getattr(pygame, KEY_BINDINGS['small_tiles']):
+            self._set_tileset('small')
             self.redraw()
-        elif event.key == pygame.K_EQUALS:
-            self._set_tileset(Tileset.large)
+        elif event.key == getattr(pygame, KEY_BINDINGS['large_tiles']):
+            self._set_tileset('large')
             self.redraw()
+        elif event.key == getattr(pygame, KEY_BINDINGS['click_swap']):
+            MOUSE_BINDINGS['reveal'], MOUSE_BINDINGS['flag'] = (
+                MOUSE_BINDINGS['flag'], MOUSE_BINDINGS['reveal'])
+        elif event.key == getattr(pygame, KEY_BINDINGS['print_console']):
+            print(self.minesweeper)
+        elif event.key == getattr(pygame, KEY_BINDINGS['help']):
+            solver = MinesweeperSolver(self.minesweeper)
+            soln = solver.solve_one()
+            if soln is not None:
+                if soln['action'] == 'flag':
+                    self.minesweeper.toggle_flag(*next(iter(soln['coords'])))
+                elif soln['action'] == 'reveal':
+                    self.minesweeper.reveal(*next(iter(soln['coords'])))
+
 
     def _set_difficulty(self, difficulty, init=False):
-        if not init and difficulty is self.difficulty:
+        if not init and difficulty == self.difficulty:
             return False
         self.difficulty = difficulty
         self.screen = None
         pygame.display.set_caption(
-            'Minesweeper [{}]'.format(self.difficulty.name))
+            'Minesweeper [{}]'.format(self.difficulty))
         return True
 
     def _set_tileset(self, tileset, init=False):
-        if not init and tileset is self.tileset:
+        if not init and tileset == self.tileset:
             return
         self.tileset = tileset
-        dir = self.tileset.value['dir']
+        dir = TILESET[self.tileset]['dir']
         self.tiles = dict()
         for file in os.listdir(dir):
             if file.endswith('.png'):
@@ -169,10 +193,10 @@ class MinesweeperView(object):
         self.screen = None
 
     def _init_screen(self):
-        tile_px = self.tileset.value['tile_px']
+        tile_px = TILESET[self.tileset]['tile_px']
         size = (
-            tile_px * self.difficulty.value['width'],
-            tile_px * self.difficulty.value['height'] + 40)
+            tile_px * DIFFICULTIES[self.difficulty]['width'],
+            tile_px * (DIFFICULTIES[self.difficulty]['height'] + 2))
         self.screen = pygame.display.set_mode(size)
 
 
@@ -203,6 +227,116 @@ class Square(object):
         )
 
 
+class MinesweeperSolver(object):
+
+    def __init__(self, minesweeper):
+        self.minesweeper = minesweeper
+
+    def _add_leasts(self, at_least, coords, mines):
+        if mines < 2 or len(coords) == mines or len(coords) < 3:
+            return
+
+        for sub_coords in itertools.combinations(coords, len(coords) - 1):
+            at_least[frozenset(sub_coords)] = mines - 1
+            self._add_leasts(at_least, sub_coords, mines - 1)
+
+    def solve_one(self):
+        m = dict()
+        m[frozenset()] = 0
+        for coord, square in self.minesweeper._squares.items():
+            if square.revealed and square.bordering > 0:
+                border_coords = self.minesweeper._get_bordering(*coord)
+                unrevealed = set()
+                flagged = set()
+                for border_coord in border_coords:
+                    border_square = self.minesweeper._squares[border_coord]
+                    if border_square.revealed:
+                        pass
+                    elif border_square.flagged:
+                        flagged.add(border_coord)
+                    else:
+                        unrevealed.add(border_coord)
+                if len(unrevealed) > 0:
+                    mines_unflagged = square.bordering - len(flagged)
+                    m[frozenset(unrevealed)] = mines_unflagged
+
+        at_least = dict()
+        at_least_free = dict()
+        for coords, mines in m.items():
+            self._add_leasts(at_least, coords, mines)
+            self._add_leasts(at_least_free, coords, len(coords) - mines)
+
+        prev_size = None
+        iters = 0
+        while len(m) + len(at_least) + len(at_least_free) != prev_size:
+            iters += 1
+            prev_size = len(m) + len(at_least) + len(at_least_free)
+            to_add = dict()
+            for coords1, mines1 in m.items():
+                for coords2, mines2 in m.items():
+                    if coords1 is not coords2:
+                        if coords1.issuperset(coords2):
+                            diff_coords = coords1 - coords2
+                            diff_mines = mines1 - mines2
+                            if diff_coords:
+                                if len(diff_coords) == diff_mines:
+                                    if iters > 1:
+                                        print('found iter {}'.format(iters))
+                                    return {'action': 'flag',
+                                            'coords': diff_coords}
+                                if diff_mines == 0:
+                                    if iters > 1:
+                                        print('found iter {}'.format(iters))
+                                    return {'action': 'reveal',
+                                            'coords': diff_coords}
+                                if diff_coords not in m:
+                                    to_add[frozenset(diff_coords)] = diff_mines
+                        if coords2.issuperset(coords1):
+                            diff_coords = coords2 - coords1
+                            diff_mines = mines2 - mines1
+                            if diff_coords:
+                                if len(diff_coords) == diff_mines:
+                                    if iters > 1:
+                                        print('found iter {}'.format(iters))
+                                    return {'action': 'flag',
+                                            'coords': diff_coords}
+                                if diff_mines == 0:
+                                    if iters > 1:
+                                        print('found iter {}'.format(iters))
+                                    return {'action': 'reveal',
+                                            'coords': diff_coords}
+                                if diff_coords not in m:
+                                    to_add[frozenset(diff_coords)] = diff_mines
+                for coords2, mines2 in at_least.items():
+                    if coords1.issuperset(coords2):
+                        diff_coords = coords1 - coords2
+                        diff_mines = mines1 - mines2
+                        if diff_coords:
+                            if diff_mines == 0:
+                                print('found LEASTS {}'.format(iters))
+                                return {'action': 'reveal',
+                                        'coords': diff_coords}
+                            elif diff_mines != len(diff_coords):
+                                self._add_leasts(at_least_free, diff_coords, len(diff_coords) - diff_mines)
+                free1 = len(coords1) - mines1
+                for coords2, free2 in at_least_free.items():
+                    if coords1.issuperset(coords2):
+                        diff_coords = coords1 - coords2
+                        diff_free = free1 - free2
+                        if diff_coords:
+                            if diff_free == 0:
+                                print('found LEASTS FREE {}'.format(iters))
+                                return {'action': 'flag',
+                                        'coords': diff_coords}
+                            elif diff_free != len(diff_coords):
+                                self._add_leasts(at_least, diff_coords, len(diff_coords) - diff_free)
+            for i, j in to_add.items():
+                m[i] = j
+            # for coords, mines in m.items():
+            #     self._add_leasts(at_least, coords, mines)
+            #     self._add_leasts(at_least_free, coords, len(coords) - mines)
+
+
 class Minesweeper(object):
 
     def __init__(self, width, height, mines):
@@ -219,6 +353,7 @@ class Minesweeper(object):
         )
         self.remaining = width * height - mines
         self.flag_remaining = mines
+        pygame.mixer.music.stop()
 
     def __str__(self):
         row_strs = []
@@ -245,6 +380,9 @@ class Minesweeper(object):
         if self.state is MinesweeperState.pregame:
             self._init_mines(x, y)
             self.state = MinesweeperState.in_progress
+            if MUSIC['enabled']:
+                pygame.mixer.music.load(MUSIC['in_game'])
+                pygame.mixer.music.play(-1)
         square = self._squares[x, y]
         if square.revealed or square.flagged:
             return
@@ -252,6 +390,9 @@ class Minesweeper(object):
         if square.is_mine:
             square.hit = True
             self.state = MinesweeperState.game_lost
+            if MUSIC['enabled']:
+                pygame.mixer.music.load(MUSIC['gameover'])
+                pygame.mixer.music.play()
             self._reveal_all()
             return
         if square.bordering == 0:
@@ -261,6 +402,9 @@ class Minesweeper(object):
         if self.remaining == 0:
             self._reveal_all()
             self.state = MinesweeperState.game_won
+            if MUSIC['enabled']:
+                pygame.mixer.music.load(MUSIC['victory'])
+                pygame.mixer.music.play()
 
     def burst_reveal(self, x, y):
         if self.state in MinesweeperState.end_states():
@@ -311,6 +455,6 @@ class Minesweeper(object):
 
 
 if __name__ == '__main__':
-    os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, 0)
+    #os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, 0)
     pygame.init()
     main()
